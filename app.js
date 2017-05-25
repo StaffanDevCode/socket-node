6fd50602386e3db860c70b91f0798036688d2444
Staffan Frisk <staffan.frisk@devcode.se>
2017-05-25 23:59:51 +0200

    socket up and running

--- app.js
+++ app.js
@@ -1,14 +1,26 @@
 const express = require('express');
 const app = express();
+const server = require('http').createServer(app);
+const io = require('socket.io')(server);
+const basePath = '/api/v1/';
 
-app.get('/', function(req, res){
+app.get(basePath, function(req, res){
   res.send({'response':'ok'});
+});
+
+app.use(express.static('public'));
+
+io.on('connection', function (socket) {
+  socket.emit('news', { hello: 'world' });
+  socket.on('my other event', function (data) {
+    console.log(data);
+  });
 });
 
 app.use(function (err, req, res, next) {
   res.status(500).send('Something broke!')
 });
 
-app.listen(3030, function () {
+server.listen(3030, function () {
   console.log('app ready on localhost:3030');
 });
